ad095c8e3841e2419c763f73a3026facbe20a8b3
JinxYTW <tyohann974@gmail.com>
2024-05-24 09:13:24 +0200

    WebServer

--- backendMe/src/App.java
+++ backendMe/src/App.java
@@ -1,17 +1,17 @@
-
 import java.sql.SQLException;
-
 import dao.ProductsDAO;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import database.PolyBayDatabase;
-
 import models.Product;
+import webserver.WebServer;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         ProductsDAO productsDAO = new ProductsDAO();
+        WebServer webServer = new WebServer();
+        webServer.listen(8080);
         
 
 
