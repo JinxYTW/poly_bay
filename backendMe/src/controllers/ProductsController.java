7a810bd838cf5258cc1c2d34bed2212402ba6a3b
JinxYTW <tyohann974@gmail.com>
2024-05-24 09:41:36 +0200

    Liste des produits

--- backendMe/src/controllers/ProductsController.java
+++ backendMe/src/controllers/ProductsController.java
@@ -2,16 +2,18 @@ package controllers;
 
 import java.util.ArrayList;
 
+import dao.ProductsDAO;
 import models.Product;
 import webserver.WebServerContext;
 import webserver.WebServerResponse;
 
 public class ProductsController {
+    private static ProductsDAO productsDAO = new ProductsDAO();
 
     public static ArrayList<Product> findAll(WebServerContext context) {
-        ArrayList<Product> products = new ArrayList<Product>();
+        ArrayList<Product> products = productsDAO.findAll();
 
-        context.getResponse().ok("Tous les produits");
+        context.getResponse().json(products);
 
         
         
